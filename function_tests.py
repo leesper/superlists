03de5f1a329fa3cc8949bacb8e1bedbf3d654b27
leesper <pascal7718@gmail.com>
2024-04-19 17:12:41 +0800

    Front page html now generated from a template

--- function_tests.py
+++ function_tests.py
@@ -23,7 +23,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), 
+                        "New to-do item did not appear in table")
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
