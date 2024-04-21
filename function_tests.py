08eaaeb9be62078d9123438dd030fb5e951c8d26
leesper <pascal7718@gmail.com>
2024-04-21 10:07:12 +0800

    commit before refactor

--- function_tests.py
+++ function_tests.py
@@ -18,13 +18,20 @@ class NewVisitorTest(unittest.TestCase):
 
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
+
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(2)
+
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(2)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows), 
-                        "New to-do item did not appear in table")
+
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
