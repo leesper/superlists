4dfe06f22bd9960e9903afc3ac009ac6ae83468f
leesper <pascal7718@gmail.com>
2024-04-29 10:19:02 +0800

    Moved Fts into their own inidividual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,34 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
-from selenium.common.exceptions import WebDriverException
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-import os
+from .base import FunctionalTest
 
 MAX_WAIT = 10
-class NewVisitorTest(StaticLiveServerTestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
-    
-    def tearDown(self):
-        self.browser.quit()
 
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() -  start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_for_one_user(self):
         # 伊迪丝听说有一个很酷的在线待办事项应用
         # 她去看了这个应用首页 
@@ -99,18 +75,4 @@ class NewVisitorTest(StaticLiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-
         # 两人都很满意，然后去睡觉了
-    def test_layout_and_styling(self):
-        # 伊迪丝访问首页
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-        # 她看到输入框完美地居中显示
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
-        # 她新建了一个清单，看到输入框仍完美地居中显示
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: testing')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
